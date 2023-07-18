1a55f1fc91f5b7f24ad2c34295313b1bdf82ddb3
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-07-18 14:36:00 +0200

    📦 NEW: Add example for the zoom id getter

---
+++ example/get_event_zoom_id.py
@@ -0,0 +1,30 @@
+import json
+import logging
+import os
+import sys
+
+from requests import HTTPError
+
+from ms_python_client.cern_ms_api_client import CERNMSApiClient
+from ms_python_client.utils.error import generate_error_log
+from ms_python_client.utils.logger import setup_logs
+
+logger = setup_logs(log_level=logging.INFO)
+
+cern_ms_client = CERNMSApiClient.init_from_dotenv()
+
+for env_var in ["USER_ID", "EVENT_ID"]:
+    if not os.getenv(env_var):
+        logger.error("%s not found in environment variables", env_var)
+        sys.exit(1)
+
+USER_ID = os.getenv("USER_ID", "")
+EVENT_ID = os.getenv("EVENT_ID", "")
+
+try:
+    result = cern_ms_client.events.get_event_zoom_id(USER_ID, EVENT_ID)
+    print(f"Zoom ID: {result}")
+
+except HTTPError as e:
+    print(json.dumps(generate_error_log(e), indent=4))
+    sys.exit(1)
